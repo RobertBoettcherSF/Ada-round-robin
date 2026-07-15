98b3be2457e596770717e351aeab6b917d282276
Sternenfisch <105643443+RobertBoettcherSF@users.noreply.github.com>
2026-07-15 17:32:44 +0200

    Create round_robin.ads

---
+++ round_robin.ads
@@ -0,0 +1,6 @@
+-- round_robin.ads
+-- Specification for the main execution procedure.
+-- In Ada, a main procedure can optionally have an .ads file. 
+-- Keeping it parameterless makes it a valid entry point for the executable.
+
+procedure Round_Robin;
